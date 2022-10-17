f45a1dad3a6c7efe0a09d8d50c9fbe48959dd87c
Nikhil <nikhil.shah2008@gmail.com>
2022-10-17 20:48:15 +0530

    Initial Commit - added a new project MVC

---
+++ shoppingAPPMVC_Demo_DevOps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shoppingAPPMVC_Demo_DevOps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
